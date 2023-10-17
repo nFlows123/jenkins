5f9ea3b5b239685108c7fbbb56f1aaa64b1d0e9b
nFlows123 <147406158+nFlows123@users.noreply.github.com>
2023-10-17 12:23:12 +0530

    Update sh1_file.sh

--- sh1_file.sh
+++ sh1_file.sh
@@ -1,2 +1,2 @@
 echo "hello"
-sudo docker build --no-cache -t stradegi001/nflowsr2:neo4j4.4.7test -f Neo4jDockerFile .
+docker build --no-cache -t stradegi001/nflowsr2:neo4j4.4.7test -f Neo4jDockerFile .
